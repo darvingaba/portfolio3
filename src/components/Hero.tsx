150b390eabfd283a0e8db607f020bd36db297a9f
Darvin <darvingaba1@gmail.com>
2023-08-11 15:39:03 +0200

    ////

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,11 +7,11 @@ function Hero() {
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
           <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-            <span className='gradientText'>Hello </span>there,
+            <span className="gradientText">Hello </span>there,
             <br />
             My name is Darvin
           </h1>
-          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+          <p className="max-w-2xl mb-6  text-gray-700 font-normal lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
             Welcome to My Software Engineering Journey
           </p>
 
@@ -21,7 +21,7 @@ function Hero() {
           >
             Get started
             <svg
-              className="w-5 h-5 ml-2 -mr-1"
+              className="w-5 h-5 ml-2 -mr-1 hover:-rotate-45"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -18,13 +18,29 @@ function Projects() {
             <span className="bg-gray-100 text-gray-900 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
               FrontEnd
             </span>
-            <h3 className="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
-              Official website
-            </h3>
-            <p className="text-lg font-normal text-gray-500 dark:text-gray-400">
-              Flowbite helps you connect with friends, family and communities of
-              people who share your interests.
-            </p>
+            
+            <div className="text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 bg-blue-200 text-blue-700 rounded-full"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="feather feather-bell-off mr-2"
+    >
+      <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+      <path d="M18.63 13A17.89 17.89 0 0 1 18 8"></path>
+      <path d="M6.26 6.26A5.86 5.86 0 0 0 6 8c0 7-3 9-3 9h14"></path>
+      <path d="M18 8a6 6 0 0 0-9.33-5"></path>
+      <line x1="1" y1="1" x2="23" y2="23"></line>
+    </svg>
+    Tag
+  </div>
             <a
               href="#"
               title=""
